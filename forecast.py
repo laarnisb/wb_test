de2c6d89800cfbe5f61b0e84bc975fd936e0b85d
laarnisb <laarnisb49@gmail.com>
2025-06-15 23:22:43 -0500

    Add files via upload

---
+++ forecast.py
@@ -0,0 +1,12 @@
+
+def forecast_spending(transactions):
+    """
+    Simulated forecast logic placeholder.
+    Returns weekly forecast amounts based on example data.
+    """
+    return {
+        "Week 1": 145.20,
+        "Week 2": 160.75,
+        "Week 3": 138.90,
+        "Week 4": 152.40
+    }
